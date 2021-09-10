73d66b84e9ffd87bceaaf9e66245b688a2e7c0cc
Kuznetsov Pavel <kuznetsovpavel1@yandex.ru>
2021-09-10 11:41:34 +0300

    +babel | handler of alternative typing letters

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,27 @@
 export const constants = {
   banWordTimeout: 300, //seconds for timeout username
+  extraCharCnt: 20,
 };
 
 export const banWords = [];
+
+export const equalChars = [
+  ["а", "a", "@"],
+  ["в", "b"],
+  ["е", "e", "з", "3", "ё", "э"],
+  ["и", "i", "u", "l", `\\|`, "й", "1"],
+  ["к", "k"],
+  ["м", "m"],
+  ["н", "h"],
+  ["о", "o", "0"],
+  ["п", "n"],
+  ["р", "p"],
+  ["с", "c"],
+  ["т", "t", "m"],
+  ["у", "y"],
+  ["х", "x"],
+  ["ч", "ch", "4"],
+  ["ь", "b"],
+  ["ы", "bl", `b\\|`, "bi", "b1", "ьl", `ь\\|`, "ьi", "ь1"],
+  ["s", "$"],
+];
--- functions/functions.js
+++ functions/functions.js
@@ -1,19 +1,38 @@
 import moment from "moment";
 
-import { banWords } from "../config/constants.js";
+import { banWords, constants, equalChars } from "../config/constants.js";
+const { extraCharCnt } = constants;
 
 export const momentDateTime = () => moment().format("DD.MM.YYYY HH:mm:ss");
 
 export const isBan = (message) => {
   const regexStr = banWords.reduce((str = "", word) => {
-    const wordMod = word
-      .split("")
-      .reduce((subStr = "", char) => `${subStr}${char}+[^a-zA-Zа-яА-Я]*`, "");
+    const wordMod = word.split("").reduce((subStr = "", char) => {
+      const iterator = (char, i) => {
+        if (i >= equalChars.length) {
+          return char;
+        } else if (equalChars[i].includes(char)) {
+          return equalChars[i].reduce(
+            (charNew = "", item) =>
+              `${charNew.length > 0 ? `${charNew}|` : ""}${item}`
+          );
+        } else {
+          return iterator(char, i + 1);
+        }
+      };
+
+      return `${subStr}(${iterator(
+        char,
+        0
+      )})+[^a-zA-Zа-яА-Я0-9]{0,${extraCharCnt}}`;
+    }, "");
 
     return `${str.length > 0 ? `${str}|` : ""}${wordMod}`;
   }, "");
 
   const regexp = new RegExp(regexStr, "gi");
 
-  return regexStr.length > 0 && regexp.test(message);
+  const res = regexp.test(message);
+
+  return regexStr.length > 0 ? res : false;
 };
